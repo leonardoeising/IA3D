c2093f4b43229c226bf73924d8f13c799b10d97b
Leonardo <112594817+leonardoeising@users.noreply.github.com>
2024-08-02 07:45:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaresultado = document.querySelector('.caixa-resultado');
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2",
+            ]
+    }
+]

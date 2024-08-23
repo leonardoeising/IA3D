bd94497b23217a3f195763af9700c9f9660745c2
Leonardo <112594817+leonardoeising@users.noreply.github.com>
2024-08-23 07:47:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
-const caixaresultado = document.querySelector('.caixa-resultado');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
 const perguntas = [
     {
-        enunciado: "pergunta 1",
-        alternativas: [
-            "alternativa 1",
-            "alternativa 2",
-            ]
+        enunciado: "Pergunta 1",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas:["Alternativa 1","Alternativa 2"]
     }
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+}
